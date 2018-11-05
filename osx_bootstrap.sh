5cd33d5f3dc8812d4fd6a44ac2ece32c2acb629b
Toby Hamand <tobyhamand@gmail.com>
2018-11-05 12:54:55 +0000

    added: NVM

--- osx_bootstrap.sh
+++ osx_bootstrap.sh
@@ -20,4 +20,8 @@ rm ~/.zshrc
 cp ./.zshrc ~/.zshrc
 cp ./.tetris_aliases ~/.tetris_aliases
 
+echo "Installing NVM"
+mkdir ~/.nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
 "UseKeychain yes" > ~/.ssh/config
