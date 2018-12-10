94bbe3bb0a310d91ae402572b590a6ea039ba3f1
Toby Hamand <tobyhamand@gmail.com>
2018-12-10 17:15:27 +0000

    adding global gitignore

--- osx_bootstrap.sh
+++ osx_bootstrap.sh
@@ -25,3 +25,5 @@ mkdir ~/.nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 "UseKeychain yes" > ~/.ssh/config
+
+git config --global core.excludesfile ./.gitignore_global
