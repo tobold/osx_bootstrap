a2e25852e7040592a99fd16479fae6aed093aa5f
Toby Hamand <tobyhamand@gmail.com>
2020-02-13 14:03:15 +0000

    symlink things properly

--- osx_bootstrap.sh
+++ osx_bootstrap.sh
@@ -11,21 +11,37 @@ else
 fi
 
 echo "Installing brew packages"
-brew bundle
+brew bundle --no-lock
 
-echo "Installing oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -x ~/.oh-my-zsh ]
+then
+  echo "Installing oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+if [ ! -x ~/.oh-my-zsh/plugins/zsh-autosuggestions ]
+then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+fi
+
+if [ ! -x ~/.nvm ]
+then
+  echo "Installing NVM"
+  mkdir ~/.nvm
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+fi
 
 rm ~/.zshrc
-cp ./.zshrc ~/.zshrc
-cp ./.tetris_aliases ~/.tetris_aliases
+ln -s "$(pwd)/.zshrc" ~/.zshrc
 
-echo "Installing NVM"
-mkdir ~/.nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+rm ~/.tetris_aliases
+ln -s "$(pwd)/.tetris_aliases" ~/.tetris_aliases
 
-"UseKeychain yes" > ~/.ssh/config
+rm ~/.ssh/config
+ln -s "$(pwd)/.sshconfig" ~/.ssh/config
 
-ln -s ./.gitignore_global ~/.gitignore_global
+#setup git ignore global and symlink
+rm ~/.gitignore_global
+ln -s "$(pwd)/.gitignore_global" ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 git config --global pull.rebase true
